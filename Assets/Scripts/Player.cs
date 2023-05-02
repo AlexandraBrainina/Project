1e763a84a796a4a5a9f7f2f1006975c2edec283a
Alexandra <brajina03@gmail.com>
2023-05-02 06:35:02 +0300

    working movement system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -9.81f;
+    public float jump = 1f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector2 _look;
+    [SerializeField] private float mouseSensetivity = 3f;
+    [SerializeField] private Transform cameraTransform;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        UpdateGravity();
+        UpdateMovement();
+        UpdateLook();
+    }
+
+    private void UpdateGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+
+    private void UpdateMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * (speed * Time.deltaTime));
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    private void UpdateLook()
+    {
+        _look.x += Input.GetAxis("Mouse X") * mouseSensetivity;
+        _look.y += Input.GetAxis("Mouse Y") * mouseSensetivity;
+
+        _look.y = Mathf.Clamp(_look.y, -89.9f, 89.9f);
+
+        transform.localRotation = Quaternion.Euler(0, _look.x, 0);
+        cameraTransform.localRotation = Quaternion.Euler(-_look.y, 0, 0);
+    }
+}
+
